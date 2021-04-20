397c31ce03952c04ef5ee6acffaa0269bbdf8a20
tomertar100 <77539388+tomertar100@users.noreply.github.com>
2021-04-20 15:02:44 +0300

    react todo app
    uploading necessary files for react todo app

---
+++ react folder/src/components/TodoList.js
@@ -0,0 +1,47 @@
+import React,{useState} from 'react';
+import Todo from './Todo';
+import TodoForm from './TodoForm';
+
+function TodoList() {
+    // eslint-disable-next-line
+    const [todos,setTodos] = useState([])
+
+    const addToDo = (todo) => {
+        if(!todo.text || /^\s*$/.test(todo.text)){
+            return;
+        }
+
+        if(todo.date === null || todo.date === ''){
+            return;
+        }
+
+        setTodos([todo,...todos])
+    }
+
+    const removeTodo = (id) => {
+        const removeArray = [...todos].filter(todo => todo.id !== id)
+        setTodos(removeArray)
+    }
+
+
+    const completeTodo = (id) => {
+        let updatedTodos = todos.map(todo => {
+            if(todo.id === id){
+                todo.isComplete = !todo.isComplete 
+            }
+            return todo
+        })
+        setTodos(updatedTodos)
+    }
+
+    return (
+        <div>
+            <h1>My To-Do List: </h1>
+            <TodoForm onSubmit = {addToDo}/>
+            <Todo todos = {todos} completeTodo = {completeTodo} removeTodo = {removeTodo} />
+            
+        </div>
+    )
+}
+
+export default TodoList
