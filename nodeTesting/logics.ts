4da402502230fe13582ca88ffb37104f79f28b56
tomertar100 <77539388+tomertar100@users.noreply.github.com>
2021-07-22 17:08:11 +0300

    another one

--- nodeTesting/logics.ts
+++ nodeTesting/logics.ts
@@ -1,22 +1,19 @@
 //helper function that finds the requested index for the array requests
-export function isIndexInArray(index,array){
-    if(typeof array[index] === 'undefined'){
-        return -1
-    }
-    else{
-        return index;
-    }
+export function isIndexInArray(index, array) {
+  if (typeof array[index] === "undefined") {
+    return -1;
+  } else {
+    return index;
+  }
 }
-
 
 //helper function
 export function checkCorrectType(value) {
-    if(typeof value === "number" || typeof value === "string"){
-        return true
-    }
-    else{
-         return false
-    }
+  if (typeof value === "number" || typeof value === "string") {
+    return true;
+  } else {
+    return false;
+  }
 }
 
-module.exports = {isIndexInArray,checkCorrectType}
+module.exports = { isIndexInArray, checkCorrectType };
