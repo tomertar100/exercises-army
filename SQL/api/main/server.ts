c59594a57bfdc0c6211a2887ae848f7a80b5857f
tomertar100 <77539388+tomertar100@users.noreply.github.com>
2021-10-20 17:10:58 +0300

    authentication

--- SQL/api/main/server.ts
+++ SQL/api/main/server.ts
@@ -2,12 +2,29 @@ import express from "express";
 import postRouter from "../routes/postRoutes";
 import userRouter from "../routes/userRoutes";
 import commentsRouter from "../routes/commentRoutes";
+import jwt from "jsonwebtoken";
+import { getAllUsers, getUser } from "../actions/userAction";
+import { authenticateToken, secretKey } from "./middleware";
 
 const port = 8000;
 const app = express();
 
 app.use(express.json());
 
+app.post("/api/login", async (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+  const loginUser = await getUser(username);
+  if (loginUser) {
+    const accessToken = jwt.sign(username, secretKey);
+    res.json({
+      username: username,
+      accessToken: accessToken,
+    });
+  }
+});
+
+app.use("/", authenticateToken);
 app.use("/api/posts", postRouter);
 app.use("/api/users", userRouter);
 app.use("/api/comments", commentsRouter);
